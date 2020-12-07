e802f131e9b0364531836f472f1b789619a98b49
xuyuzhiyue <240011530@qq.com>
2020-12-07 14:38:53 +0800

    增加buffer的读取方式

--- file/01-readFIle.js
+++ file/01-readFIle.js
@@ -2,7 +2,12 @@
 const fs = require('fs')
 
 // 2.调用模块api
-fs.readFile('../file/1.txt', (err, data) => {
-    console.log(err, 'err');
-    console.log(data.toString, 'data');
+// path 读取的路径
+// options 可选的参数默认为null    其实会输出 buffer 二进制 可以用data.toString转换
+// data读取数据
+fs.readFile('../file/1.txt', 'utf8',(err, data) => {
+    if(err){
+        throw '数据返回错误'
+    }
+    console.log(data,'成功读取文件');
 }) 
